4dc3b9aab0f43f9e62d6da9139ba87dae108ac15
AndrewDanial <adanial9379@gmail.com>
2023-02-28 21:48:32 -0500

    Got rid of toolbar and changed chart height

--- client/src/components/StateTab.js
+++ client/src/components/StateTab.js
@@ -24,71 +24,74 @@ export default function StateTab() {
     store.setStateNoDistrict(event.target.value, false);
   };
 
-  let chartStuff= {
-        series: [{
-          name: 'Democrat',
-          data: [44]
-        }, {
-          name: 'Republican',
-          data: [53]
-        }, {
-          name: 'Open Seats',
-          data: [0,12]
-        }],
-        options: {
-          chart: {
-            type: 'bar',
-            stacked: true,
-          },
-          colors: ['#0096FF','#FF3131','#cccccc'],
-          plotOptions: {
-            bar: {
-              horizontal: true,
-              dataLabels: {
-                total: {
-                  enabled: true,
-                  offsetX: 0,
-                  style: {
-                    fontSize: '20px',
-                    fontWeight: 'bold',
-                    color: '#FFFFFF'
-                  }
-                }
-              }
-            },
-          },
-          xaxis: {
-            categories: ["Safe Seats", "Open Seats"],
-            labels: {
-              style:{
-                colors: '#FFFFFF'
-              }
-            }
-          },
-          yaxis: {
-            labels: {
-              style:{
-                colors: '#FFFFFF',
-                fontSize: '20px',
-                fontWeight: 'bold'
-              }
-            }
-          },
+  let chartStuff = {
+    series: [{
+      name: 'Democrat',
+      data: [44]
+    }, {
+      name: 'Republican',
+      data: [53]
+    }, {
+      name: 'Open Seats',
+      data: [0, 12]
+    }],
+    options: {
+      chart: {
+        type: 'bar',
+        stacked: true,
+        toolbar: {
+          show: false
+        }
+      },
+      colors: ['#0096FF', '#FF3131', '#cccccc'],
+      plotOptions: {
+        bar: {
+          horizontal: true,
           dataLabels: {
-            style: {
-              fontSize: '20px',
-              fontWeight: 'bold'
-            }
-          },
-          legend: {
-            position: 'top',
-            horizontalAlign: 'right',
-            offsetX: 40,
-            labels: {
-              colors: '#FFFFFF'
+            total: {
+              enabled: true,
+              offsetX: 0,
+              style: {
+                fontSize: '13px',
+                fontWeight: 'bold',
+                color: '#FFFFFF'
+              }
             }
           }
         },
+      },
+      xaxis: {
+        categories: ["Safe Seats", "Open Seats"],
+        labels: {
+          style: {
+            colors: '#FFFFFF',
+          }
+        }
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: '#FFFFFF',
+            fontSize: '13px',
+            fontWeight: 'bold'
+          }
+        }
+      },
+      dataLabels: {
+        style: {
+          fontSize: '13px',
+          fontWeight: 'bold'
+        }
+      },
+      legend: {
+        position: 'top',
+        horizontalAlign: 'right',
+        offsetX: 40,
+        labels: {
+          colors: '#FFFFFF'
+        }
+      }
+    },
 
 
   };
@@ -110,29 +113,29 @@ export default function StateTab() {
         </Select>
       </FormControl>
       {/*move this stuff to a separate window since this is ensemble summary*/}
-      <Box display='none' sx={{fontFamily:'Arial', fontSize: '11', marginTop: 2, marginBottom: 2}}>
-            <h1>Ensemble Summary Data</h1>
-            <b> Number of District Plans: </b> {"use store.currentState to get nums"} <br/>
-            <b> Number of Incumbents: </b> {"use store.currentState to get nums"} <br/>
-            <b> Incumbents Predicted to Win: </b> {"use store.currentState to get nums"} <br/>
-            <b> Average Geographic Variation in Incumbent Districts: </b> {"use store.currentState to get nums"} <br/>
-            <b> Average Population Variation in Incumbent Districts: </b> {"use store.currentState to get nums"} <br/>
-        </Box>
+      <Box display='none' sx={{ fontFamily: 'Arial', fontSize: '11', marginTop: 2, marginBottom: 2 }}>
+        <h1>Ensemble Summary Data</h1>
+        <b> Number of District Plans: </b> {"use store.currentState to get nums"} <br />
+        <b> Number of Incumbents: </b> {"use store.currentState to get nums"} <br />
+        <b> Incumbents Predicted to Win: </b> {"use store.currentState to get nums"} <br />
+        <b> Average Geographic Variation in Incumbent Districts: </b> {"use store.currentState to get nums"} <br />
+        <b> Average Population Variation in Incumbent Districts: </b> {"use store.currentState to get nums"} <br />
+      </Box>
       {/*everything below this shud be district plan summary, e.g list of incumbents, num of districts, etc*/}
-      <Box display={store.currentState==""? 'none': 'block'}>
-        <Box sx={{fontFamily:'Arial', fontSize: '11', marginTop: 2, marginBottom: 2}} >
-              <b> Number of Districts: </b> {"nums dependent on currentState and currentPlan"} <br/>
-              <b> Number of Incumbents: </b> {"nums dependent on currentState and currentPlan"} <br/>
-              <b> Incumbent District Variation: </b> {"nums dependent on currentState and currentPlan"} <br/>
-          </Box>
+      <Box display={store.currentState == "" ? 'none' : 'block'}>
+        <Box sx={{ fontFamily: 'Arial', fontSize: '11', marginTop: 2, marginBottom: 2 }} >
+          <b> Number of Districts: </b> {"nums dependent on currentState and currentPlan"} <br />
+          <b> Number of Incumbents: </b> {"nums dependent on currentState and currentPlan"} <br />
+          <b> Incumbent District Variation: </b> {"nums dependent on currentState and currentPlan"} <br />
+        </Box>
         <IncumbentTable ></IncumbentTable>
         <ReactApexChart
           options={chartStuff.options}
           series={chartStuff.series}
           type="bar"
           width="100%"
-          height="90%"
-      ></ReactApexChart>
+          height="80%"
+        ></ReactApexChart>
       </Box>
     </Container>
 
