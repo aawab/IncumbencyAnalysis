6dff909679d9fef934188a64a83d0e37fc2100b0
Aawab M <aawab.mahmood@stonybrook.edu>
2023-03-01 02:41:07 -0500

    F

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -59,6 +59,123 @@ function RenderMap() {
 
     const { store } = useContext(GlobalStoreContext);
 
+    // SETUP DATA ACCORDING TO PLAN AND STATE
+
+    function createData(districtNum, incumbent, party, result, geoVar, popVar) {
+        return { districtNum, incumbent, party, result, geoVar, popVar };
+      }
+
+    let rows = []
+    if (store.currentState == "Ohio")
+    {
+        if (store.currentPlan == "2020")
+        {
+        rows = 
+        [
+        createData(1, "Steve Chabot", "R", "W", 0.9, 1.1),
+        createData(2, "Brad Wenstrup", "R", "W", 0.8, 1.2),
+        createData(3, "Joyce Beatty", "D", "W", 0.6, 1.9),
+        createData(4, "Jim Jordan", "R", "W", 0.4, 1.2),
+        createData(5, "Bob Latta", "R", "W", 0.6, 1.1),
+        createData(6, "Bill Johnson", "R", "W", 0.7, 1.3),
+        createData(7, "Bob Gibbs", "R", "W", 1.1, 0.8),
+        createData(8, "Warren Davidson", "R", "W", 0.7, 0.9),
+        createData(9, "Marcy Kaptur", "D", "W", 0.9, 0.6),
+        createData(10, "Michael Turner", "R", "W", 0.9, 1.1),
+        createData(11, "Marcia Fudge", "D", "W", 1.2, 1.3),
+        createData(12, "Troy Balderson", "R", "W", 1.3, 1.5),
+        createData(13, "Tim Ryan", "D", "W", 0.8, 1.7),
+        createData(14, "David Joyce", "R", "W", 0.8, 0.8),
+        createData(15, "Steve Stivers", "R", "W", 0.7, 0.9),
+        createData(16, "Anthony Gonzalez", "R", "W", 0.7, 0.9)
+        ]
+        }
+        else //2022 and other plans
+        {
+        rows = 
+        [
+        createData(1, "Steve Chabot", "R", "L", 0.9, 1.1),
+        createData(2, "Brad Wenstrup", "R", "W", 0.8, 1.2),
+        createData(3, "Joyce Beatty", "D", "W", 0.6, 1.9),
+        createData(4, "Jim Jordan", "R", "W", 0.4, 1.2),
+        createData(5, "Bob Latta", "R", "W", 0.6, 1.1),
+        createData(6, "Bill Johnson", "R", "W", 0.7, 1.3),
+        createData(7, "Bob Gibbs", "R", "W", 1.1, 0.8),
+        createData(8, "Warren Davidson", "R", "W", 0.7, 0.9),
+        createData(9, "Marcy Kaptur", "D", "W", 0.9, 0.6),
+        createData(10, "Michael Turner", "R", "W", 0.9, 1.1),
+        createData(11, "Shontel Brown", "D", "W", 1.2, 1.3),
+        createData(12, "Troy Balderson", "R", "W", 1.3, 1.5),
+        createData(13, "Anthony Gonzalez", "R", "W", 0.8, 1.7),
+        createData(14, "David Joyce", "R", "W", 0.8, 0.8),
+        createData(15, "Mike Carey", "R", "W", 0.7, 0.9)
+        ]
+        }
+    }
+    else if (store.currentState == "Arizona")
+    {
+        if (store.currentPlan == "2020")
+        {
+        rows =
+        [
+        createData(1, "Tom O'Halleran", "D", "W", 0.9, 1.1),
+        createData(2, "Ann Kirkpatrick", "D", "W", 0.8, 1.2),
+        createData(3, "Raul Grijalva", "D", "W", 0.6, 1.9),
+        createData(4, "Paul Gosar", "R", "W", 0.4, 1.2),
+        createData(5, "Andy Biggs", "R", "W", 0.6, 1.1),
+        createData(6, "David Schweikert", "R", "W", 0.7, 1.3),
+        createData(7, "Ruben Gallego", "D", "W", 1.1, 0.8),
+        createData(8, "Debbie Lesko", "R", "W", 0.7, 0.9),
+        createData(9, "Greg Stanton", "D", "W", 0.9, 0.6)
+        ]
+        }
+        else //2022 and other plans
+        {
+        rows =
+        [
+        createData(1, "David Schweikert", "R", "W", 0.9, 1.1),
+        createData(2, "Tom O'Halleran", "D", "L", 0.8, 1.2),
+        createData(3, "Ruben Gallego", "D", "W", 0.6, 1.9),
+        createData(4, "Greg Stanton", "D", "W", 0.4, 1.2),
+        createData(5, "Andy Biggs", "R", "W", 0.6, 1.1),
+        createData(6, "Ann Kirkpatrick", "D", "L", 0.7, 1.3),
+        createData(7, "Raul Grijalva", "D", "W", 1.1, 0.8),
+        createData(8, "Debbie Lesko", "R", "W", 0.7, 0.9),
+        createData(9, "Paul Gosar", "R", "W", 0.9, 0.6)
+        ]
+        }
+    }
+    else if (store.currentState == "Colorado")
+    {
+        if (store.currentPlan == "2020")
+        {
+        rows =
+        [
+        createData(1, "Diana DeGette", "D", "W", 0.9, 1.1),
+        createData(2, "Joe Neguse", "D", "W", 0.8, 1.2),
+        createData(3, "Scott Tipton", "R", "W", 0.6, 1.9),
+        createData(4, "Ken Buck", "D", "W", 0.4, 1.2),
+        createData(5, "Doug Lamborn", "R", "W", 0.6, 1.1),
+        createData(6, "Jason Crow", "D", "W", 0.7, 1.3),
+        createData(7, "Ed Perlmutter", "D", "W", 1.1, 0.8)
+        ]
+        }
+        else //2022 and other plans
+        {
+        rows =
+        [
+        createData(1, "Diana DeGette", "D", "W", 0.9, 1.1),
+        createData(2, "Joe Neguse", "D", "W", 0.8, 1.2),
+        createData(3, "Lauren Boebert", "R", "W", 0.6, 1.9),
+        createData(4, "Ken Buck", "D", "W", 0.4, 1.2),
+        createData(5, "Doug Lamborn", "R", "W", 0.6, 1.1),
+        createData(6, "Jason Crow", "D", "W", 0.7, 1.3),
+        createData(7, "Ed Perlmutter", "D", "W", 1.1, 0.8),
+        createData(8, "Yadira Caraveo", "D", "W", 0.7, 0.9)
+        ]
+        }
+    }
+
     // HIGHLIGHT ON HOVER
     function highlightArea(e) {
         e.target.setStyle({
@@ -114,7 +231,8 @@ function RenderMap() {
     // STYLE DISTRICTS BASED ON SELECTION
     function districtStyle(district) {
         let color = "#FFFFF"
-        store.currentDistrict == parseInt(district.properties.DISTRICT)?color = "#fcba03":color = "#0000FF"
+        color=rows[district.properties.DISTRICT-1].party=="R"?"#FF3131":"#0096FF"
+        store.currentDistrict == parseInt(district.properties.DISTRICT)?color = "#fcba03":color=color
         return {
             fillColor: color,
             color: color,
@@ -220,7 +338,10 @@ function RenderMap() {
                     renderSwitch()
                 }
             </MapContainer >
-            <Toolbar sx={{ position: 'absolute', top: '0%', marginTop: "1%", right: '0%', marginRight: "1%", width: '15%', height: '10%', background: '#202124', opacity: 0.8, boxShadow: 2 }}/>
+            {/* <Box sx={{ textAlign: 'right',position: 'absolute', padding:'1%',top: '0%', marginTop: "1%", right: '0%', marginRight: "1%", width: '15%', background: '#202124', opacity: 0.8, boxShadow: 2 }}>
+                <b>US Map</b> <br/> 
+                Click on a state to get started! <br/>
+            </Box> */}
         </Box >
 
 
