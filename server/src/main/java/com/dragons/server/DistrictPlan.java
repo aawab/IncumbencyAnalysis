570563d56f04134cfd16a1ac6d303cd695e741f8
Aawab M <aawab.mahmood@stonybrook.edu>
2023-04-26 18:29:25 -0400

    distplanupdated

--- server/src/main/java/com/dragons/server/DistrictPlan.java
+++ server/src/main/java/com/dragons/server/DistrictPlan.java
@@ -14,6 +14,7 @@ public class DistrictPlan {
     private District[] districts;
 
     private int numIncumbents;
+    private int incumbentDistrictVariation;
     
     private String incumbentTable;
     private String safeSeatGraph;
