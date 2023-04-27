5d3788e807bd74a0b4abb740a45edd1d616bac4e
AndrewDanial <adanial9379@gmail.com>
2023-04-26 21:10:17 -0400

    Some seawulf stuff exists, though it hangs after 3 iterations

--- backend/seawulf.py
+++ backend/seawulf.py
@@ -4,10 +4,10 @@ from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
 from gerrychain.proposals import recom
 from functools import partial
 import pandas
-
+import geopandas as gp
 def gen_initial_partition(state):
     
-    graph = Graph.from_file(f"./preprocessing/{state}precincts.json")
+    graph = Graph.from_file(f"./preprocessing/{state}precincts.geojson")
     elections = [
         Election("PRES20", {"Democratic": "adv_20", "Republican": "arv_20"})
     ]
@@ -39,49 +39,28 @@ def gen_initial_partition(state):
                    node_repeats=2
                 )
     
-    # compactness_bound = constraints.UpperBound(
-    #     lambda p: len(p["cut_edges"]),
-    #     2*len(initial_partition["cut_edges"])
-    # )
+    compactness_bound = constraints.UpperBound(
+        lambda p: len(p["cut_edges"]),
+        2*len(initial_partition["cut_edges"])
+    )
 
     pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.07)
     
     chain = MarkovChain(
         proposal=proposal,
         constraints=[
-            #compactness_bound
+            compactness_bound
         ],
         accept=accept.always_accept,
         initial_state=initial_partition,
-        total_steps=3
+        total_steps=30
     )
     
     for partition in chain:
         data = pandas.DataFrame(
-            sorted(partition["PRES20"].percents("Democratic"))
+             sorted(partition["PRES20"].percents("Democratic"))
         )   
-        print(data)
-    print(partition['exterior_boundaries'])
-    #a = partition["geometries"]
-    fig, ax = plt.subplots(figsize=(8, 6))
-
-    # Draw 50% line
-    ax.axhline(0.5, color="#cccccc")
-
-    # Draw boxplot
-    data.boxplot(ax=ax, positions=range(len(data.columns)))
-
-    # Draw initial plan's Democratic vote %s (.iloc[0] gives the first row)
-    plt.plot(data.iloc[0], "ro")
-
-    # Annotate
-    ax.set_title("Comparing the 2011 plan to an ensemble")
-    ax.set_ylabel("Democratic vote % (Senate 2012)")
-    ax.set_xlabel("Sorted districts")
-    ax.set_ylim(0, 1)
-    ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
-
-    plt.show()
+        print(partition)
 
 def main():
     gen_initial_partition('az')
